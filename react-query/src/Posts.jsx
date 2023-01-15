abd4bf1c37c77a24468e5c73319633560a8bd74d
seongHo <tlatjdgh3778@github.com>
2023-01-15 11:18:01 +0900

    staleTIme, cacheTime

--- react-query/src/Posts.jsx
+++ react-query/src/Posts.jsx
@@ -3,45 +3,49 @@ import { useQuery } from '@tanstack/react-query';
 
 import { PostDetail } from './PostDetail';
 
-
 const maxPostPage = 10;
 
 const fetchPosts = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0")
+  const response = await fetch(
+    'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
+  );
 
-  return response.json()
-}
+  return response.json();
+};
 
 const Posts = () => {
-  const [currentPage, setCurrentPage] = useState(0)
-  const [selectedPost, setSelectedPost] = useState(null)
+  const [currentPage, setCurrentPage] = useState(0);
+  const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with react query
-  const { data, isError, error, isLoading } = useQuery(["posts"], fetchPosts);
+  const { data, isError, error, isLoading } = useQuery(['posts'], fetchPosts, {
+    staleTime: 2000,
+  });
 
-  if(isLoading) return <h3>Loading ... </h3>
+  if (isLoading) return <h3>Loading ... </h3>;
 
-  if(isError) return (
-    <>
-    <h3>Oops, something went wrong</h3>
-    <p>{error.toString()}</p>
-    </>
-  )
+  if (isError)
+    return (
+      <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
-       <ul>
+      <ul>
         {data.map((post) => (
           <li
             key={post.id}
-            className="post-title"
+            className='post-title'
             onClick={() => setSelectedPost(post)}
           >
             {post.title}
           </li>
         ))}
       </ul>
-      <div className="pages">
+      <div className='pages'>
         <button disabled onClick={() => {}}>
           Previous page
         </button>
