307b1260f5ddf4ce03da4ed5ddff79e14240da27
seongHo <tlatjdgh3778@github.com>
2023-01-15 10:34:41 +0900

    commit

---
+++ react-query/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+
+import './App.css';
+import Posts from './Posts';
+
+function App() {
+  return (
+    // Provide React Query client to App
+    <div className='App'>
+      <h1>Blog &apos;em Ipsum</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-query/src/Posts.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+
+const maxPostPage = 10;
+
+const fetchPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0")
+
+  return response.json()
+}
+
+const Posts = () => {
+  const [currentPage, setCurrentPage] = useStatee(0)
+  const [selectedPost, setSelectedPost] = useStatee(null)
+
+  // replace with react query
+  const data = [];
+
+  return (
+    <>
+       <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button disabled onClick={() => {}}>
+          Previous page
+        </button>
+        <span>Page {currentPage + 1}</span>
+        <button disabled onClick={() => {}}>
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+};
+
+export default Posts;
