aec041c3778616d75d54cbf2fa62a87a840a4121
seongHo <tlatjdgh3778@github.com>
2023-01-15 11:11:14 +0900

    react-query-devools 설치

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Posts from './Posts';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -9,10 +10,11 @@ function App() {
   return (
     // Provide React Query client to App
     <QueryClientProvider client={queryClient}>
-    <div className='App'>
-      <h1>Blog &apos;em Ipsum</h1>
-      <Posts />
-    </div>
+      <div className='App'>
+        <h1>Blog &apos;em Ipsum</h1>
+        <Posts />
+      </div>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
