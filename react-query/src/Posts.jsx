f215709575cd812c2c6b07ee17df6c1c02b07ce0
seongHo <tlatjdgh3778@github.com>
2023-01-18 21:43:29 +0900

    prefetching

--- react-query/src/Posts.jsx
+++ react-query/src/Posts.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { PostDetail } from './PostDetail';
+import { useEffect } from 'react';
 
 const maxPostPage = 10;
 
@@ -17,12 +18,27 @@ const Posts = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    // 10페이지에 있다면 프리페칭 필요없음
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      // 다음 페이지의 정보를 미리 가져온다.
+      // 사용자 경험 증가 ~
+      queryClient.prefetchQuery(['posts', nextPage], () =>
+        fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
+
   // replace with react query
   const { data, isError, error, isLoading } = useQuery(
     ['posts', currentPage],
     () => fetchPosts(currentPage),
     {
       staleTime: 2000,
+      keepPreviousData: true,
     }
   );
 
