9841fe31ae1ef9e0f7c90f8a4d53667be8b3224d
seongHo <tlatjdgh3778@github.com>
2023-02-06 23:36:01 +0900

    queryClient default options

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -5,7 +5,7 @@ import { theme } from '../theme';
 
 const toast = createStandaloneToast({ theme });
 
-function queryErrorHandler(error: unknown): void {
+export function queryErrorHandler(error: unknown): void {
   // error is type unknown because in js, anything can be an error (e.g. throw(5))
   const id = 'react-query-error';
   const title =
