268d6ccfef4c6859a3822511f985a36a91c25af2
seongHo <tlatjdgh3778@github.com>
2023-02-06 23:21:59 +0900

    onError queryClient

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,6 @@ import { useQuery } from '@tanstack/react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -11,18 +10,8 @@ async function getTreatments(): Promise<Treatment[]> {
   return data;
 }
 export function useTreatments(): Treatment[] {
-  const toast = useCustomToast();
-
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
-    onError: (error) => {
-      const title =
-        error instanceof Error
-          ? error.message
-          : 'error connecting to the server';
-      toast({ title, status: 'error' });
-    },
-  });
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
 
   return data;
 }
