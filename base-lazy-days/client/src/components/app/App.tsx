baac7f77c5a4063c29090ddee91d95d381d9975e
seongHo <tlatjdgh3778@github.com>
2023-02-06 23:07:15 +0900

    useTreatment

--- base-lazy-days/client/src/components/app/App.tsx
+++ base-lazy-days/client/src/components/app/App.tsx
@@ -1,6 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactElement } from 'react';
 
+import { queryClient } from '../../react-query/queryClient';
 import { theme } from '../../theme';
 import { Loading } from './Loading';
 import { Navbar } from './Navbar';
@@ -9,9 +12,12 @@ import { Routes } from './Routes';
 export function App(): ReactElement {
   return (
     <ChakraProvider theme={theme}>
-      <Navbar />
-      <Loading />
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Loading />
+        <Routes />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,3 +1,5 @@
+import { QueryClient } from '@tanstack/react-query';
+
 // import { createStandaloneToast } from '@chakra-ui/react';
 // import { theme } from '../theme';
 
@@ -15,4 +17,5 @@
 // }
 
 // to satisfy typescript until this file has uncommented contents
-export {};
+
+export const queryClient = new QueryClient();
