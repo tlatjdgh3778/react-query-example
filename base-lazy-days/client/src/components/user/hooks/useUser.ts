a554fc6b798c5ff1b3c291817b5319f0c6dbf658
seongHo <tlatjdgh3778@github.com>
2023-02-13 22:46:38 +0900

    useUser 1

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,3 +1,4 @@
+import { useQuery } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 
 import type { User } from '../../../../../shared/types';
@@ -9,16 +10,16 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${user.id}`,
-//     {
-//       headers: getJWTHeader(user),
-//     },
-//   );
-//   return data.user;
-// }
+async function getUser(user: User | null): Promise<User | null> {
+  if (!user) return null;
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      headers: getJWTHeader(user),
+    },
+  );
+  return data.user;
+}
 
 interface UseUser {
   user: User | null;
@@ -27,8 +28,7 @@ interface UseUser {
 }
 
 export function useUser(): UseUser {
-  // TODO: call useQuery to update user data from server
-  const user = null;
+  const { data: user } = useQuery(queryKeys.user, () => getUser(user));
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
