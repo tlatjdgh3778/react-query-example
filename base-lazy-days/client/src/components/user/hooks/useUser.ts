3951d8b886ca960ca804fe5596936a9d8eba43e5
seongHo <tlatjdgh3778@github.com>
2023-02-13 23:14:49 +0900

    setQueryData

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 
 import type { User } from '../../../../../shared/types';
@@ -11,6 +11,7 @@ import {
 } from '../../../user-storage';
 
 async function getUser(user: User | null): Promise<User | null> {
+  console.log('getUser');
   if (!user) return null;
   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
     `/user/${user.id}`,
@@ -28,16 +29,28 @@ interface UseUser {
 }
 
 export function useUser(): UseUser {
-  const { data: user } = useQuery(queryKeys.user, () => getUser(user));
+  const queryClient = useQueryClient();
+  const { data: user } = useQuery(queryKeys.user, () => getUser(user), {
+    onSuccess: (data: User | null) => {
+      if (!data) {
+        clearStoredUser();
+      } else {
+        setStoredUser(data);
+      }
+    },
+  });
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
-    // TODO: update the user in the query cache
+    console.log(newUser);
+    setStoredUser(newUser);
+    queryClient.setQueryData(queryKeys.user, newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
-    // TODO: reset user to null in query cache
+    clearStoredUser();
+    queryClient.setQueryData(queryKeys.user, null);
   }
 
   return { user, updateUser, clearUser };
